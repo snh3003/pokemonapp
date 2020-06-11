8ab94752d6e4c0a40a4a60e5163892ff86e24db9
snh3003 <shahsama542@gmail.com>
2020-06-11 14:23:43 +0530

    Updates

---
+++ precache-manifest.0dd6e6020e6e3c2b477c16e92ae696e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98a3375fcdd848997ff29be9369d026a",
+    "url": "/pokemonapp/index.html"
+  },
+  {
+    "revision": "9a0aa9571dff933cfa20",
+    "url": "/pokemonapp/static/css/main.030541a2.chunk.css"
+  },
+  {
+    "revision": "d545e50d7de3c3132e6e",
+    "url": "/pokemonapp/static/js/2.d0a60094.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/pokemonapp/static/js/2.d0a60094.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a0aa9571dff933cfa20",
+    "url": "/pokemonapp/static/js/main.2bf10c8c.chunk.js"
+  },
+  {
+    "revision": "81baac3868d37d8d8190",
+    "url": "/pokemonapp/static/js/runtime-main.79e8b53a.js"
+  }
+]);
